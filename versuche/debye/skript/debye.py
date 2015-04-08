849adeb46f1678937e5f0255ef46c41c2d103eb0
Dominik12345 <dominik.kahl@tu-dortmund.de>
2015-04-08 18:41:11 +0200

    Debye-Scherrer weitergemacht, zweite Messreihe?? Tabellen noch nicht richtig formatiert

--- versuche/debye/skript/debye.py
+++ versuche/debye/skript/debye.py
@@ -178,8 +178,8 @@ R=57.3 #in mm
 t1=r1/(2.*R)
 t2=r2/(2.*R)
 
-print t1
-
+print str(t1) + "t1" 
+print str(t2) + "t2"
 #Bragg: nl=2dsint , Ann:n=_1 ? => d= l/2sint
 d11=l1/(2.*np.sin(t1))
 d12=l1/(2.*np.sin(t2))
@@ -196,15 +196,16 @@ s2=4.*s2/(s2[0])
 print "h**2+k**2+l**2 =:s"
 print "s1" + str(s1) + "passt zu bcc"
 print "s2" + str(s2) + "passt zu diamant"
+#exp. Berechnung mit theor. Werten vergleichen, um abzuschätzen welchen Wert für s man nehmen muss
 
-rs1=np.sqrt(s1)
-rs2=np.sqrt(s2)
-
-
-#erste Berechnungsmöglichkeit
 print "Die Netzebenenabstände ergeben:"
 print "d1" + str(d11)
 print "d2" + str(d12)
+
+sth1=[4,6,8,10,16,20,22]
+sth2=[4,11]
+rs1=np.sqrt(sth1)
+rs2=np.sqrt(sth2)
 
 a1=d11*rs1
 a2=d12*rs2
@@ -218,30 +219,96 @@ print "a2" + str(a2)
 
 # es gilt exakt l=2 a/sqrt(s) sin(t) <=> a=l*sqrt(s)
 
+a1_uc = ev.get_uncert(a1)
+a2_uc = ev.get_uncert(a2)
+
+print a1_uc
+print a2_uc
+
+#Regression a gegen cos^2 t 
+
+
+def G(x, m, b):
+    return m*x + b
+
+val1, std1 = ev.fit(G, np.cos(t1)**2,a1 )
+val2, std2 = ev.fit(G, np.cos(t1)**2,a1 )  ###
+
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+
+ax1.plot(np.cos(t1)**2, a1, linestyle = 'none', marker = '+', label = 'Messwerte')
+lim = ax1.get_xlim()
+x = np.linspace(lim[0], lim[1], 1000)
+ax1.plot(x, G(x, val1[0], val1[1]), label="Fit")
+
+ax1.set_xlabel(r'$\cos^2(\vartheta)$')
+ax1.set_ylabel(r'$a / \si{\meter}$')
+
+ax1.legend(loc = 'best')
+ax1 = ev.plot_layout(ax1)
+
+fig1.tight_layout()
+
+#######
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+
+ax2.plot(np.cos(t1)**2, a1, linestyle = 'none', marker = '+', label = 'Messwerte')
+lim = ax2.get_xlim()
+x = np.linspace(lim[0], lim[1], 1000)
+ax2.plot(x, G(x, val2[0], val2[1]), label="Fit")
+
+ax2.set_xlabel(r'$\cos^2(\vartheta)$')
+ax2.set_ylabel(r'$a / \si{\meter}$')
+
+ax2.legend(loc = 'best')
+ax2 = ev.plot_layout(ax2)
+
+fig2.tight_layout()
+
+
+#Bilder speichern
+fig1.savefig('../tex/bilder/fig1.pdf')
+fig2.savefig('../tex/bilder/fig2.pdf')
 
 
 
+# LateX-Tabelle erzeugen
+table1 = lt.latextable(
+    [r1,t1,s1,sth1, a1],
+    "table",
+    alignment = 'CCCCC',
+    form = '.3f',
+)
+table2 = lt.latextable(
+    [r1,t1,s1,sth1, a1],
+    "table",
+    alignment = 'CCCCC',
+    form = '.3f',
+)
 
+ev.write('../tex/tabellen/tabelle1.tex', table1)
+ev.write('../tex/tabellen/tabelle2.tex', table2)
 
+# latex-Gleichung der linearen Regression
+lin_reg1 = ev.tex_linreg(
+        "G_1(t)",
+        val1,
+        std1,
+        unit = ["", "\meter"]
+)
 
+ev.write('../tex/gleichungen/gerade1.tex', lin_reg1)
 
+lin_reg2 = ev.tex_linreg(
+        "G_2(t)",
+        val2,
+        std2,
+        unit = ["", "\meter"]
+)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ev.write('../tex/gleichungen/gerade2.tex', lin_reg2)
 
 
